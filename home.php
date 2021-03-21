7075625e99f06f5bd80af082d93fa62fc162f3a6
Asha jyothi Donga <58396795+asha24102001@users.noreply.github.com>
2021-03-21 10:58:30 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+ <!-- code of html and add this in script part -->
+<!-- example -->
+<script>
+    $(function()
+      { $("#navid").load("./nav-footer/nav.html");
+        $("#footid").load("./nav-footer/nav.html");
+      });
+</script>
+ <!-- this divs load the nav and footer html codes -->
+<div id="navid"></div>
+<div id="footerid"></div>

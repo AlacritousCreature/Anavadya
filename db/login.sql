16dc7b224e0b7438007fed5a9a8a377fd461a57d
Asha jyothi Donga <58396795+asha24102001@users.noreply.github.com>
2021-05-17 14:17:59 +0530

    Update login.sql

--- db/login.sql
+++ db/login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2021 at 10:02 AM
+-- Generation Time: May 17, 2021 at 10:46 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -24,15 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `roomoccupncy`
+--
+
+CREATE TABLE `roomoccupncy` (
+  `timeVal` datetime NOT NULL,
+  `roomno` int(11) NOT NULL,
+  `strength` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
+  `userimg` longtext NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+  `email` mediumtext NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `roomno` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `userimg`, `password`, `email`, `created_at`, `roomno`) VALUES
+(20, 'admin', 'https://cdn.corporatefinanceinstitute.com/assets/macro-manager-1024x683.jpeg', '$2y$10$98CkaDVx5hxykcJAq7N5XubnHY/yunEB.cg83UIgk7WFTtDPik3cW', 'admin@gmail.com', '2021-05-17 13:35:57', 0);
 
 --
 -- Indexes for dumped tables
@@ -52,7 +74,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
